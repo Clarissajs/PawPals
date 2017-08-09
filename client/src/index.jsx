46980193b11de29552b0ea13017ba21590b88545
Jason Stieber <pharovall@Pharrovall.local>
2017-08-08 17:21:22 -0700

    starting template

---
+++ client/src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render () {
+    return (<div>
+      <h1>PawPals</h1>
+    </div>)
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
