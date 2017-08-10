472ff0a34cb8a9f04e1616b05aed675b1f656c82
Spencer Mitchell <smitch.work@gmail.com>
2017-08-10 15:41:31 -0700

    Implement browser icon and licensing

--- client/src/App.js
+++ client/src/App.js
@@ -29,6 +29,7 @@ class App extends Component {
             <Route exact path='/signup' component={Signup} />
           </Switch>
         </div>
+        <div style={{fontSize: 7}}>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
       </div>
     );
   }
