6595ce79fd7bff363e59d3697fe65755a7b605fe
Spencer Mitchell <smitch.work@gmail.com>
2017-08-17 16:35:58 -0700

    Implement redux Signup form handling

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Input, Form, FormGroup, Button, Col } from 'reactstrap';
+import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+
+import userActions from '../actions/userActions'
+import CustomInput from './CustomInput'
 
 class Signup extends React.Component {
+
+  handleSubmit(userData) {
+    this.props.signupUser(userData);
+  }
 
   render() {
     return (
@@ -9,35 +18,35 @@ class Signup extends React.Component {
         <h2 style={{
           marginBottom: 15
         }}>Signup</h2>
-        <Form>
+        <Form onSubmit={this.props.handleSubmit(this.handleSubmit.bind(this))}>
 
           <FormGroup row>
             <Col xs={{size: 10, offset: 1}}>
-              <Input type='email' id='emailField' placeholder='Email'/>
+              <Field name='email' type='email' placeholder='Email' component={CustomInput}/>
             </Col>
           </FormGroup>
 
           <FormGroup row>
             <Col xs={{size: 10, offset: 1}}>
-              <Input type='text' id='firstNameField' placeholder='First Name'/>
+              <Field name='firstName' type='text' placeholder='First Name' component={CustomInput}/>
             </Col>
           </FormGroup>
 
           <FormGroup row>
             <Col xs={{size: 10, offset: 1}}>
-              <Input type='text' id='lastNameField' placeholder='Last Name'/>
+              <Field name='lastName' type='text' placeholder='Last Name' component={CustomInput}/>
             </Col>
           </FormGroup>
 
           <FormGroup row>
             <Col xs={{size: 10, offset: 1}}>
-              <Input type='password' id='passwordField' placeholder='Password'/>
+              <Field name='password' type='password' placeholder='Password' component={CustomInput}/>
             </Col>
           </FormGroup>
 
           <FormGroup row>
             <Col xs={{size: 10, offset: 1}}>
-              <Input type='password' id='passwordField' placeholder='Confirm Password'/>
+              <Field name='confirmPassword' type='password' placeholder='Confirm Password' component={CustomInput}/>
             </Col>
           </FormGroup>
 
@@ -52,5 +61,24 @@ class Signup extends React.Component {
     )
   }
 }
+
+// Connect Signup to redux store
+
+const mapStateToProps = state => state.user;
+
+const mapDispatchToProps = dispatch => {
+  return {
+    signupUser: (userData) => {
+      dispatch(userActions.signupUser(userData));
+    }
+  }
+}
+
+Signup = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Signup);
+
+Signup = reduxForm({form: 'Signup'})(Signup);
 
 export { Signup };
