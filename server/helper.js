63e3536d9af95ec4c8b4a5973fc72a6c3d8d7539
Jason Stieber <pharovall@Pharrovall.local>
2017-08-19 15:09:32 -0700

    removed promisifying

--- server/helper.js
+++ server/helper.js
@@ -1,10 +1,10 @@
 const crypto = require('crypto');
-const db = Promise.promisifyAll(require('../database-mongo'));
+const db = require('../database-mongo');
 
 
 var setCookieSession = function(username) {
   let cookie = crypto.randomBytes(32).toString('hex');
-  // db.saveNewCookie(cookie, username);
+  db.saveNewCookie(cookie, username);
   return cookie;
 }
 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Promise = require('bluebird');
 const bodyParser = require('body-parser');
-const db = Promise.promisifyAll(require('../database-mongo'));
+const db = require('../database-mongo');
 const sha256 = require('sha256');
 const crypto = require('crypto');
 const helper = require('./helper.js');
@@ -29,7 +29,9 @@ app.get('/', (req, res) => {
 });
 
 app.get('/listings', (req,res) => {
-
+  db.retrieveAllUsers((users => {
+    res.send(users);
+  }))
 
 });
 
@@ -37,31 +39,26 @@ app.get('/listings', (req,res) => {
 app.get('/login', (req, res) => {
   var username = req.param('username').toUpperCase();
   var password = req.param('password');
-  db.userExistsAsync(username)
-    .then((exists) => {
-      if(!exists){
-        throw new Error ('Useranme does not exist');
-      }
-    })
-    .then(() => {
-      return db.grabUserDataAsync(username)
-    })
-    .then(userData => {
-      if(sha256(userData.salt + password) === userData.hashPass){
-        console.log('we are logged in');
-        alert('you have logged in');
-        var cookie = helper.setCookieSession(userData.username);
-        res.cookie('session',cookie, { maxAge: 900000, httpOnly: true });
-        console.log('cookie created successfully');
-      } else{
-        console.log("that isn't the correct Password ");
-        res.redirect('/login');
-      }
-    })
-    .catch(e => {
-      console.log(e);
-      res.send(400);
-    });
+  db.userExists(username, (err, exists) => {
+
+    if(!exists){
+      throw new Error ('Useranme does not exist');
+    } else {
+      db.grabUserData(username, (err, person) => {
+        if(sha256(userData.salt + password) === userData.hashPass){
+          console.log('we are logged in');
+          alert('you have logged in');
+          var cookie = helper.setCookieSession(userData.username);
+          console.log(cookie);
+          res.cookie('session',cookie, { maxAge: 900000, httpOnly: true });
+          console.log('cookie created successfully');
+        } else{
+          console.log("that isn't the correct Password ");
+          res.redirect('/login');
+        }
+      });
+    }
+  });
 });
 
 app.post('/signup', (req, res) => {
@@ -69,28 +66,23 @@ app.post('/signup', (req, res) => {
   var pass = req.query.password;
   throw('we have requsted a signup');
   var email = req.query.email;
-  db.userExistsAsync(username,cb)
-    .then((exists) => {
-      if(exists){
-        throw new Error ('Useranme already taken');
-      }
-    })
-    .then(() => {
-        let salt = crypto.randomBytes(32).toString('hex');
-        let hashPass = sha256(salt+pass);
-        db.saveNewUserAsync(JSON.stringify({username, email, hashPass, salt})); // make sure this lines up with claiassas entry function
-        // svar cookie = helper.setCookieSession(username);
-        alert(helper.setCookieSession(username));
-        res.cookie('session',cookie, { maxAge: 900000, httpOnly: true });
-        console.log('cookie created successfully');
-      })
-    .then(() => {
-      res.send(201);
-    })
-    .catch((e) =>{
-        console.log(e); // Holy Errors Batman
-        res.send(400);
-    })
+  userExists(username, (err, exists) => {
+    if(exists){
+      throw new Error ('Useranme already taken');
+      res.redirect('/singup');
+      res.send(302); //
+    } else {
+      let salt = crypto.randomBytes(32).toString('hex');
+      let hashPass = sha256(salt+pass);
+      db.saveNewUser(JSON.stringify({username: username, email: email, hashPass: hashPass, salt:salt})); // make sure this lines up with claiassas entry function
+      var cookie = helper.setCookieSession(username);
+      alert(helper.setCookieSession(username));
+      res.cookie('session',cookie, { maxAge: 900000, httpOnly: true });
+      console.log('cookie created successfully');
+    }
+
+  });
+    res.send(201);
 });
 
 app.listen(3000, function() {
